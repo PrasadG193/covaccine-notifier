9a54d31db61676eba94907ce2b882ae5448cd5df
Noel Georgi <git@frezbo.dev>
2021-06-12 22:34:42 +0530

    Add Mattermost notifier
    Signed-off-by: Noel Georgi <git@frezbo.dev>

--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.16
 
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
-	github.com/pkg/errors v0.8.1
+	github.com/mattermost/mattermost-server/v5 v5.35.3
+	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.1.3
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 )
---
+++ pkg/notify/mattermost.go
@@ -0,0 +1,50 @@
+package notify
+
+import (
+	"fmt"
+
+	mattermost "github.com/mattermost/mattermost-server/v5/model"
+)
+
+type Mattermost struct {
+	Client    *mattermost.Client4
+	ChannelID string
+}
+
+// NewMattermost returns an instance of an authenticated
+// *mattermost.Client4 and the mattermost direct message channel ID
+func NewMattermost(url, token, username string) (Notifier, error) {
+	client := mattermost.NewAPIv4Client(url)
+	client.AuthToken = token
+	client.AuthType = mattermost.HEADER_AUTH
+	client.HttpHeader = map[string]string{
+		"Authorization": fmt.Sprintf("Bearer %s", token),
+	}
+	botUser, res := client.GetMe("")
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("unable to authenticate bot user: %s, using token", botUser.Nickname)
+	}
+	sendUser, res := client.GetUserByUsername(username, "")
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("unable to find user id of user: %s", username)
+	}
+	directChannel, res := client.CreateDirectChannel(botUser.Id, sendUser.Id)
+	if res.StatusCode != 201 {
+		return nil, fmt.Errorf("unable to crete direct message between botUser: %s and %s", botUser.Nickname, username)
+	}
+	return &Mattermost{
+		Client:    client,
+		ChannelID: directChannel.Id,
+	}, nil
+}
+
+// SendMessage sends the message to a mattermost user as a bot
+func (m *Mattermost) SendMessage(body string) error {
+	if _, res := m.Client.CreatePost(&mattermost.Post{
+		ChannelId: m.ChannelID,
+		Message:   body,
+	}); res.StatusCode != 201 {
+		return fmt.Errorf("error sending message to channel: %s", m.ChannelID)
+	}
+	return nil
+}
